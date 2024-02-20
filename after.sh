e12ed0bce658126133178cfc45835716743365bd
Santanu Rajbhar <66637268+Santanu-10@users.noreply.github.com>
2024-02-20 16:54:18 +0530

    Update after.sh

--- after.sh
+++ after.sh
@@ -1,3 +1,3 @@
 docker pull santanurajbhar/docker-python-app:latest
 
-docker run -d --name myCon --image santanurajbhar/docker-python-app:latest
+docker run -d --image santanurajbhar/docker-python-app:latest
